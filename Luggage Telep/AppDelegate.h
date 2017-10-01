48dfc894492507b68fe4c96274bc12597bb79c6b
MacOS <mobile.worldev@gmail.com>
2017-10-01 04:31:22 +0300

    Initial Commit

---
+++ Luggage Telep/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Luggage Telep
+//
+//  Created by MacOS on 10/1/17.
+//  Copyright © 2017 MacOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Luggage Telep/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Luggage Telep
+//
+//  Created by MacOS on 10/1/17.
+//  Copyright © 2017 MacOS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
